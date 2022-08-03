7655acf863a836d754ecad9b849aa5a360328e4e
Nguyen Marc <nguyen_marc@live.fr>
2022-08-03 23:08:47 +0200

    fix: remove privileged podman
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- katago.sh
+++ katago.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-sudo podman run --rm -it \
+podman run --rm -it \
   -v "$(pwd)/default_gtp.cfg:/app/default_gtp.cfg:ro" \
   -v "$(pwd)/default_model.bin.gz:/app/default_model.bin.gz" \
   --security-opt=label=disable \
-  --privileged \
   --hooks-dir=/usr/share/containers/oci/hooks.d/ \
   docker.io/darkness4/katago:latest \
   $@
