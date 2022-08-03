b45768ae9da965a347792c8c1c0d0679fc63083b
Nguyen Marc <nguyen_marc@live.fr>
2022-08-03 20:09:35 +0200

    feat: prefer podman over docker
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- katago.sh
+++ katago.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-docker run --rm --gpus all -i \
+podman run --rm -i \
   -v "$(pwd)/default_gtp.cfg:/app/default_gtp.cfg:ro" \
   -v "$(pwd)/default_model.bin.gz:/app/default_model.bin.gz" \
-  --device /dev/nvidia0 --device /dev/nvidia-uvm --device /dev/nvidia-uvm-tools --device /dev/nvidiactl \
+  --security-opt=label=disable \
+  --hooks-dir=/usr/share/containers/oci/hooks.d/ \
   docker.io/darkness4/katago:latest \
   $@
